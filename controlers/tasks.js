73d6ec43bbc571393f6a6447be57b0e43a68eda8
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-02-18 13:17:39 +0100

    task opertaions

--- controlers/tasks.js
+++ controlers/tasks.js
@@ -1,11 +1,64 @@
-const handleTasks = (db) => (req,res) => {
-  return (
-    <div>
-      
-    </div>
-  )
+const task = require('../models/task');
+
+const getTasks = (db) => (req,res) => {
+  const { userid } = req.query;
+  db.select('*')
+  .from('tasks')
+  .where('userid', '=', userid)
+  .then(tasks => res.status(200).json(tasks))
+  .catch(err => res.status(400).json(err, ':', 'unable to get data'))
+}
+
+const getTask = (db) => (req,res) => {
+  const { userid } = req.query;
+  db.select('*')
+  .from('tasks')
+  .where('id', '=', parseInt(req.params.id), '&',  'userid', '=', userid)
+  .then(tasks => res.status(200).json(tasks))
+  .catch(err => res.status(400).json(err, ':', 'unable to get data'))
+}
+
+const addTask = (db) => (req,res) => {
+  const { userid, task, category, priority } = req.query;
+  db.insert(
+    { userid: userid, 
+      task:task, 
+      category:category, 
+      priority:priority, 
+      timestamp: new Date()
+    })
+  .into('tasks')
+  .then(a => res.status(200).json('new tasks added'))
+  .catch(err => res.status(400).json(err, ':', 'unable to add data'))
+}
+
+const deleteTask = (db) => (req,res) => {
+  db('tasks')
+  .where('id', '=', parseInt(req.params.id))
+  .del()
+  .then(a => res.status(200).json('task deleted'))
+  .catch(err => res.status(400).json(err, ':', 'unable to delete data'))
+}
+
+const updateTask = (db) => (req,res) => {
+  const { userid, task, category, priority } = req.query;
+  db('tasks')
+  .where('id', '=', req.params.id)
+  .update(
+    { userid: userid, 
+      task:task, 
+      category:category, 
+      priority:priority, 
+      timestamp: new Date()
+    })
+  .then(tasks => res.status(200).json(tasks))
+  .catch(err => res.status(400).json(err, ':', 'unable to get data'))
 }
 
 module.exports = {
-    handleTasks: handleTasks
+    getTasks: getTasks,
+    addTask: addTask,
+    getTask: getTask,
+    deleteTask: deleteTask,
+    updateTask: updateTask
 }
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
 const cors = require('cors');
+const pg = require('pg');
 const app = express();
 const signin = require('./controlers/signin');
 const register = require('./controlers/register');
 const tasks = require('./controlers/tasks');
+
 
 const db = knex({
     client: 'pg',
@@ -23,7 +25,11 @@ app.use(cors());
 
 app.post('/signin', signin.handleSignin(db,bcrypt));
 app.post('/register', register.handleRegister(db,bcrypt));
-app.post('/tasks', tasks.handleTasks(db));
+app.get('/tasks', tasks.getTasks(db));
+app.get('/tasks/:id', tasks.getTask(db));
+app.post('/tasks', tasks.addTask(db));
+app.delete('./tasks/:id', tasks.deleteTask(db));
+app.put('./tasks/:id', tasks.updateTask(db));
 
 app.listen(3001 || process.env.PORT, ()=>{
     console.log(`app is running on port  ${process.env.PORT}`);
